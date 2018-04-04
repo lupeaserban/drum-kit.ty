f1dd3e7fa50bd03baf5761febfd3f4a9b77e6182
Serban Lupea <lupea.serban@gmail.com>
2018-04-03 20:22:11 -0700

    added main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+
+  function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop the function from running
+    audio.currentTime=0; //revind to start
+    audio.play();
+    key.classList.add('playing');
+  }
+
+  function removeTransition(e){
+    if(e.propertyName !== 'transform')return; //skip if its not a transform
+    this.classList.remove('playing')
+  }
+
+  const keys = document.querySelectorAll('.key')
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+  window.addEventListener('keydown', playSound);
+
+  // drum kit button
+  const button= document.querySelector('h1');
+  function playCat(e){
+    const audioCat= document.getElementById('cat');
+    if(!audioCat) return;
+    audioCat.currentTime=0;
+    audioCat.play();
+    button.classList.add('playing');
+  }
+
+  button.addEventListener('transitionend', removeTransition);
+  button.addEventListener('click', playCat);
